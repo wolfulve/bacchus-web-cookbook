e62847148000303aa5963e86d98fb5f5f0367999
Eric Bloom <eric@bacchus.com>
2016-08-05 16:44:35 -0700

    add recipe def

--- bacchus-web/metadata.rb
+++ bacchus-web/metadata.rb
@@ -5,3 +5,8 @@ license 'all_rights'
 description 'Installs/Configures bacchus-web'
 long_description 'Installs/Configures bacchus-web'
 version '0.1.0'
+
+#recipe "php-tagr::default","Upgrades php to 5.6. Does a System apt-get update"
+#recipe "php-tagr::configure","Configures for php "
+recipe "bacchus-web::deploy","Adds more deploy configuration for Java Server "
+#depends "apache2"
