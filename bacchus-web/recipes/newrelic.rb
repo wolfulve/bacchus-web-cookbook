ff077b21fce99c33ed974f57630273a87880fd6b
Eric Bloom <eric@bacchus.com>
2016-08-19 13:17:03 -0700

    template and recipe

---
+++ bacchus-web/recipes/newrelic.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: systems
+# Recipe:: newrelic
+#
+# Copyright (C) 2015 Frequency
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+
+execute "what am i" do
+    time = Time.now.to_i
+    cwd '/tmp'
+    command "mkdir -p freq-cb_systems_newrelic_#{time}"
+    action :run
+end
+
+execute "install new relic repo" do
+    cwd '/tmp'
+    command "rm -rf /tmp/newrelic-repo-5-3.noarch.rpm && rm -rf /etc/yum.repos.d/newrelic.repo && yum -y remove newrelic-repo-5-3 newrelic-sysmond && rm -rf /etc/newrelic/* && aws s3 cp s3://freq-cb-repo/newrelic-repo-5-3.noarch.rpm . && rpm -Uvh /tmp/newrelic-repo-5-3.noarch.rpm"
+    action :run
+end
+
+execute "install new relic via yum" do
+    command "yum -y install newrelic-sysmond"
+    action :run
+end
+
+execute "configure new relic" do
+    command "nrsysmond-config --set license_key=221e63f2ee0ed178aac7c2e3de018e5f26febbe9"
+    action :run
+end
+
+template "/etc/newrelic/nrsysmond.cfg" do
+    source "nrsysmond.cfg.erb"
+    owner "newrelic"
+    group "newrelic"
+    mode 0644
+end
+
+service "newrelic-sysmond" do
+    action [:enable, :restart]
+end
