ea6e09d2f1c4d022ea245eeefd6b1ebbb0be94c4
Eric Bloom <eric@bacchus.com>
2016-08-25 11:52:38 -0700

    rename

--- bacchus-web/recipes/sethostname.rb
+++ bacchus-web/recipes/sethostname.rb
@@ -4,13 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-require 'resolv'
-
-template '/etc/hosts' do
-    source "hosts.erb"
-    mode "0644"
-    variables(:opsworks => node[:opsworks])
-end
 
 execute "set hostname" do
     user 'root'
