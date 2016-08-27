1b28e79652bae12a865572749e74ae9ec2420a49
Eric Bloom <eric@bacchus.com>
2016-08-26 20:22:17 -0700

    edit

--- bacchus-web/recipes/newrelic-agent.rb
+++ bacchus-web/recipes/newrelic-agent.rb
@@ -16,6 +16,24 @@ node[:deploy].each do |application, deploy|
     end
 end
 
+#
+# todo: execute conditionally based on layer type, e.g., java-app, nodejs-app etc
+# start agent install
+execute "fetch agent and unzip" do
+    cwd '/tmp'
+    package = 'newrelic-java.zip'
+    command "aws s3 cp s3://elasticbeanstalk-us-west-2-227102987351/bacchus/#{package} . && unzip -o #{package}"
+    action :run
+end
+
+# set app name
+template "/tmp/newrelic/newrelic.yml" do
+    source "newrelic.yml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+end
+
 execute "copy agent to tomcat base" do
     cwd '/usr/share/tomcat7'
     command "rm -rf newrelic && mv /tmp/newrelic ."
--- bacchus-web/recipes/newrelic.rb
+++ bacchus-web/recipes/newrelic.rb
@@ -15,23 +15,23 @@ execute "what am i" do
     action :run
 end
 
+##
+## todo: execute conditionally based on layer type, e.g., java-app, nodejs-app etc
+## start agent install
+#execute "fetch agent and unzip" do
+#    cwd '/tmp'
+#    package = 'newrelic-java.zip'
+#    command "aws s3 cp s3://elasticbeanstalk-us-west-2-227102987351/bacchus/#{package} . && unzip -o #{package}"
+#    action :run
+#end
 #
-# todo: execute conditionally based on layer type, e.g., java-app, nodejs-app etc
-# start agent install
-execute "fetch agent and unzip" do
-    cwd '/tmp'
-    package = 'newrelic-java.zip'
-    command "aws s3 cp s3://elasticbeanstalk-us-west-2-227102987351/bacchus/#{package} . && unzip -o #{package}"
-    action :run
-end
-
-# set app name
-template "/tmp/newrelic/newrelic.yml" do
-    source "newrelic.yml.erb"
-    owner "root"
-    group "root"
-    mode 0644
-end
+## set app name
+#template "/tmp/newrelic/newrelic.yml" do
+#    source "newrelic.yml.erb"
+#    owner "root"
+#    group "root"
+#    mode 0644
+#end
 
 
 #execute "copy agent to tomcat base" do
