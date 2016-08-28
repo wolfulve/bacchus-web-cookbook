767816740fcdccda62004c590df22606fa1178d0
Eric Bloom <eric@bacchus.com>
2016-08-28 12:22:24 -0700

    edit

--- bacchus-web/recipes/newrelic-java-agent.rb
+++ bacchus-web/recipes/newrelic-java-agent.rb
@@ -29,7 +29,7 @@ end
 #end
 
 remote_file '/tmp/newrelic-java.zip' do
-    source 'http://github.com/wolfulve/bacchus-web-cookbook/blob/master/bacchus-web/newrelic-java-3.31.1.zip'
+    source 'https://github.com/wolfulve/bacchus-web-cookbook/blob/master/bacchus-web/newrelic-java-3.31.1.zip'
     owner 'root'
     group 'root'
     mode '0644'
