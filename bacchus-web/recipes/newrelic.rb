f2a49ff2e728afcbf2bec28bd0a78538784c6321
Eric Bloom <eric@bacchus.com>
2016-09-13 21:33:08 -0700

    update

--- bacchus-web/recipes/newrelic.rb
+++ bacchus-web/recipes/newrelic.rb
@@ -58,11 +58,14 @@ ruby_block "something" do
 #        Chef::Log.info("******** nodeVal: #{node[:xyz]}")
         json = command_out.stdout
         obj = JSON.parse(json)
+        
+        
+        
         server_id = obj["servers"][0]["id"]
         server_name = obj["servers"][0]["name"]
         Chef::Log.info("******** Server Id: #{server_id} Name: #{server_name} #{obj} #{server_id}")
 #        get policy for server
-        command = "curl -X GET 'https://api.newrelic.com/v2/alert_policies.json' -H 'X-Api-Key:5209987e383b241f4958ff40652fb88dc69b81526febbe9'"
+        command = "curl -X GET 'https://api.newrelic.com/v2/alert_policies.json' -H 'X-Api-Key:5209987e383b241f4958ff40652fb88dc69b81526febbe9' -d 'filter[name]=test-stack4-magic2'"
         json = shell_out(command)
         obj = JSON.parse(json)
         Chef::Log.info("******** policies: #{obj}")
