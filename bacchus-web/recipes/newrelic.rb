87642035bcd265f3fdd2bf548dddce83f30c3488
Eric Bloom <eric@bacchus.com>
2016-09-13 21:14:24 -0700

    update

--- bacchus-web/recipes/newrelic.rb
+++ bacchus-web/recipes/newrelic.rb
@@ -45,6 +45,8 @@ service "newrelic-sysmond" do
     action [:enable, :restart]
 end
 
+require 'json'
+
 ruby_block "something" do
     block do
         #tricky way to load this Chef::Mixin::ShellOut utilities
@@ -52,13 +54,19 @@ ruby_block "something" do
 #        command = "curl -X GET 'https://api.newrelic.com/v2/servers.json' -H 'X-Api-Key:b45db701025ac3714fa93428a7d3f3fbf3f604abbe56a79' -d 'filter[name]=dev-freq-collection-blueberry'"
         command = "curl -X GET 'https://api.newrelic.com/v2/servers.json' -H 'X-Api-Key:5209987e383b241f4958ff40652fb88dc69b81526febbe9' -d 'filter[name]=test-stack4-magic2'"
         command_out = shell_out(command)
-        node.set['xyz'] = command_out.stdout
-        Chef::Log.info("******** nodeVal: #{node[:xyz]}")
+#        node.set['xyz'] = command_out.stdout
+#        Chef::Log.info("******** nodeVal: #{node[:xyz]}")
         json = command_out.stdout
         obj = JSON.parse(json)
         server_id = obj["servers"][0]["id"]
         server_name = obj["servers"][0]["name"]
         Chef::Log.info("******** Server Id: #{server_id} Name: #{server_name} #{obj} #{server_id}")
+#        get policy for server
+        command = "curl -X GET 'https://api.newrelic.com/v2/alert_policies.json' -H 'X-Api-Key:5209987e383b241f4958ff40652fb88dc69b81526febbe9' -d 'filter[name]=test-stack4-magic2'"
+        json = shell_out(command)
+        obj = JSON.parse(json)
+        Chef::Log.info("******** policies: #{obj}")
+        
     end
     action :create
 end
