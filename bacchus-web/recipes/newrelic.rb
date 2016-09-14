78ff632e444ba8832b788791cfee7bec61390428
Eric Bloom <eric@bacchus.com>
2016-09-14 13:45:25 -0700

    update

--- bacchus-web/recipes/newrelic.rb
+++ bacchus-web/recipes/newrelic.rb
@@ -63,7 +63,7 @@ ruby_block "add the server id to the associated policy list" do
         command_out = shell_out(command)
         json = command_out.stdout
         obj = JSON.parse(json)
-        num_servers = obj['alert_policies'][0]['links'][0]['servers'].size;
+        num_servers = obj['alert_policies'][0]['links']['servers'].size;
         Chef::Log.info("******** policies: #{obj} #{num_servers}")
 #       add server id to list of servers
 #       post JSON back to newrelic
