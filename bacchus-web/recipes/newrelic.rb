b2573be394c037d710ccb8f7c37309f7cfbc69bf
Eric Bloom <eric@bacchus.com>
2016-09-13 18:54:10 -0700

    update

--- bacchus-web/recipes/newrelic.rb
+++ bacchus-web/recipes/newrelic.rb
@@ -61,14 +61,14 @@ execute "set new relic server policy (in-progress)" do
 end
 
 
-#require 'json'
-#
-#json = File.read('/tmp/newrelic-server.json')
-#obj = JSON.parse(json)
-#server_id = obj["servers"][0]["id"]
-#server_name = obj["servers"][0]["name"]
-#
-#Chef::Log.info("******** Server Id: #{server_id} Name: #{server_name} #{obj} #{server_id}")
+require 'json'
+
+json = File.read('/tmp/newrelic-server.json')
+obj = JSON.parse(json)
+server_id = obj["servers"][0]["id"]
+server_name = obj["servers"][0]["name"]
+
+Chef::Log.info("******** Server Id: #{server_id} Name: #{server_name} #{obj} #{server_id}")
 
 #obj["servers"].each_with_index do |server, index|
 #    Chef::Log.info("******** server data: #{server} #{server['id]'} #{server['name']}")
