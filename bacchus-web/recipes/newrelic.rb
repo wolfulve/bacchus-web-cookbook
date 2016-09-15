5d0fac02753e3e24782c236dd92eee10f643415f
Eric Bloom <eric@bacchus.com>
2016-09-14 17:49:17 -0700

    update

--- bacchus-web/recipes/newrelic.rb
+++ bacchus-web/recipes/newrelic.rb
@@ -90,8 +90,8 @@ ruby_block "add the server id to the associated policy list" do
                              Chef::Log.info("******** server id assoicated with policy: #{server_id}")
                         end
                     # build JSON here
-#                    command = "curl -X POST 'https://api.newrelic.com/v2/alert_policies/#{policy_id}' -H 'X-Api-Key:5209987e383b241f4958ff40652fb88dc69b81526febbe9' -H 'Content-Type: application/json' -d '#{update_policy}'"
-#                    command_out = shell_out(command)
+                    command = "curl -X POST 'https://api.newrelic.com/v2/alert_policies/#{policy_id}' -H 'X-Api-Key:5209987e383b241f4958ff40652fb88dc69b81526febbe9' -H 'Content-Type: application/json' -d '#{update_policy}'"
+                    command_out = shell_out(command)
                     Chef::Log.info("******** curl command: curl -X POST 'https://api.newrelic.com/v2/alert_policies/#{policy_id}' -H 'X-Api-Key:5209987e383b241f4958ff40652fb88dc69b81526febbe9' -H 'Content-Type: application/json' -d '#{update_policy}'")
                     end
                 else
